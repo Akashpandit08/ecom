26bce12de6ffb1071caa9bc54e479eac6c6cc75a
Akashpandit08 <akashsharma17722@gmail.com>
2024-09-24 22:47:37 +0530

    userSchema

---
+++ src/models/User.model.js
@@ -0,0 +1,82 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // Fix typo
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    profile: {
+      type: String,
+    },
+    role: {
+      type: Boolean, // Admin role = true, User role = false
+      default: false, // Default role as User
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Middleware to hash the password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  
+  this.password = await bcrypt.hash(this.password, 10); // Fix typo bcrypt.has to bcrypt.hash
+  next();
+});
+
+// Method to compare passwords
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Method to generate access token
+UserSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+// Method to generate refresh token
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export default mongoose.model("User", UserSchema);
